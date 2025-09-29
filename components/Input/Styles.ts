ee3faded45057b7d9a395c780f3013ad51f52c38
arthur <arthurdedsec@gmail.com>
2025-09-29 20:46:23 -0300

    Feat: criação do component input global

---
+++ components/Input/Styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+
+    InputContainer: {
+        marginBottom: 40,
+        marginTop: 10,
+        alignSelf: 'center',
+    },
+
+    label: {
+        color: "#fff",
+        marginBottom: 10,
+    },
+
+    input: {
+        backgroundColor: "transparent",
+        borderColor: "#3b82f6",
+        borderWidth: 1,
+        borderRadius: 8,
+        color: "#fff",
+        width: 300,
+        gap: 40,
+
+    },
+
+});
+
+export default Styles;
